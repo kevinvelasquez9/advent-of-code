180663e3341e575356759e121249a6de6c083988
Kevin Velasquez <velasquezkevin@icloud.com>
2023-12-03 18:59:29 -0800

    day2 and day3

--- 2023/days/day_2.py
+++ 2023/days/day_2.py
@@ -1,9 +1,31 @@
 from abstract_day import AbstractDay
 
 
+def _max_color_counts(sets):
+    mr = mg = mb = 0
+    for s in sets.split("; "):
+        m = {"red": 0, "green": 0, "blue": 0}
+        for draw in s.split(", "):
+            amt, color = draw.split(" ")
+            m[color] += int(amt)
+        mr, mg, mb = max(mr, m["red"]), max(mg, m["green"]), max(mb, m["blue"])
+    return mr, mg, mb
+
+
 class Day2(AbstractDay):
     def part1(self):
-        pass
+        total = 0
+        for line in self.lines:
+            game, sets = line.split(": ")
+            game = int(game.split(" ")[-1])
+            mr, mg, mb = _max_color_counts(sets)
+            total += game if mr <= 12 and mg <= 13 and mb <= 14 else 0
+        return total
 
     def part2(self):
-        pass
+        total = 0
+        for line in self.lines:
+            _, sets = line.split(": ")
+            mr, mg, mb = _max_color_counts(sets)
+            total += mr * mg * mb
+        return total
---
+++ 2023/days/day_3.py
@@ -0,0 +1,46 @@
+from abstract_day import AbstractDay
+
+
+def _explore(grid, r, c):
+    total = 0
+    hits = []
+    for dr in [-1, 0, 1]:
+        for dc in [-1, 0, 1]:
+            rr, cc = r + dr, c + dc
+            if 0 <= rr < len(grid) and 0 <= cc < len(grid[rr]) and grid[rr][cc].isdigit():
+                part_num = _get_part_num(grid[rr], cc)
+                total += part_num
+                hits.append(part_num)
+    return total, hits[0] * hits[1] if len(hits) == 2 else 0
+
+
+def _get_part_num(row, c):
+    row_ = row.copy()
+    left = right = c
+    while left > 0 and row[left - 1].isdigit():
+        left -= 1
+        row[left] = '.'
+    while right < len(row) - 1 and row[right + 1].isdigit():
+        right += 1
+        row[right] = '.'
+    return int(''.join(row_[left:right + 1]))
+
+
+class Day3(AbstractDay):
+    def part1(self):
+        total, _ = self._solve()
+        return total
+
+    def part2(self):
+        _, total = self._solve()
+        return total
+
+    def _solve(self):
+        part1 = part2 = 0
+        grid = [[c for c in line] for line in self.lines]
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                if not grid[r][c].isdigit() and grid[r][c] != '.':
+                    d1, d2 = _explore(grid, r, c)
+                    part1, part2 = part1 + d1, part2 + d2
+        return part1, part2
