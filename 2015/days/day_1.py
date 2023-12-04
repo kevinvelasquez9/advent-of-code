8ce38a8f90ce8efa83e09327e3535a75e52fcecf
Kevin Velasquez <velasquezkevin@icloud.com>
2023-12-04 10:44:19 -0800

    2015 day 1

---
+++ 2015/days/day_1.py
@@ -0,0 +1,15 @@
+from abstract_day import AbstractDay
+
+
+class Day1(AbstractDay):
+    def part1(self):
+        dirs = {"(": 1, ")": - 1}
+        return sum([dirs[c] for c in self.input])
+
+    def part2(self):
+        dirs = {"(": 1, ")": - 1}
+        floor = 0
+        for i, c in enumerate(self.input):
+            floor += dirs[c]
+            if floor < 0:
+                return i + 1
